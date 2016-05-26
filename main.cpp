15bc5e0ddbe653584720c15bec69d97bae6534fa
James W. Barnett <jbarnet4@tulane.edu>
2016-05-26 08:03:05 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * Test particle insertion
+ * James W. Barnett
+ * May 26, 2016
+ * 
+ */
+
+#include "gmxcpp/Trajectory.h"
+using namespace std;
+
+int main(int argc, char* arg[])
+{
+
+
+    return 0;
+
+}
