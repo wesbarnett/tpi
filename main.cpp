05104c742b2c497505529f46eb053f388906b130
James W. Barnett <jbarnet4@tulane.edu>
2016-05-26 08:06:54 -0500

    read in configuration file

--- main.cpp
+++ main.cpp
@@ -5,12 +5,27 @@
  * 
  */
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 #include "gmxcpp/Trajectory.h"
 using namespace std;
 
-int main(int argc, char* arg[])
+int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        cout << "ERROR: Configuration file should be first command line argument." << endl;
+        return -1;
+    }
 
+    cout << "Using '" << argv[1] << "' for configuration file..." << endl;
+
+    boost::property_tree::ptree pt;
+    boost::property_tree::ini_parser::read_ini(argv[1], pt);
+    char *endptr;
+
+    const string xtcfile = pt.get<std::string>("xtcfile","prd.xtc");
+    cout << "xtcfile = " << xtcfile << endl;
 
     return 0;
 
