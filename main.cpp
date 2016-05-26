e66a808f5ba80d824f5ffa32f8c4e766ff611205
James W. Barnett <jbarnet4@tulane.edu>
2016-05-26 12:22:59 -0500

    better output

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@
  * Limitations:
  *  - Must use a cubic box (does not have to be equal on all sides)
  *  - Particle being inserted has no charge, so electrostatics excluded
+ *  - Every atom type needs to have its own index group!
  */
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <random>
 
@@ -67,8 +71,8 @@ double tail(Atomtype at, double rc2, double vol);
 
 int main(int argc, char* argv[])
 {
-
-    const int block_n = 5;
+    chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now(); 
 
     /* BEGIN CONFIGURATION FILE PARSING */
 
@@ -84,11 +88,28 @@ int main(int argc, char* argv[])
     boost::property_tree::ini_parser::read_ini(argv[1], pt);
     char *endptr;
 
+    const string outfile = pt.get<std::string>("outfile","chempot.dat");
+    cout << "outfile = " << outfile << endl;
+
+    ofstream ofs(outfile.c_str());
+    ofs << scientific << setprecision(6) << left;
+
+    ofs << "-----------------------------------------------------------------------" << endl;
+    ofs << "Test particle insertion program -- Wes Barnett" << endl;
+    ofs << "-----------------------------------------------------------------------" << endl;
+    time_t start_time = chrono::system_clock::to_time_t(start);
+    ofs << setw(40) << "Started computation at:" << setw(20) << ctime(&start_time);
+
+    ofs << setw(40) << "Configuration file:" << setw(20) << argv[1] << endl;
+    ofs << setw(40) << "Output file:" << setw(20) << outfile << endl;
+
     const string xtcfile = pt.get<std::string>("xtcfile","prd.xtc");
     cout << "xtcfile = " << xtcfile << endl;
+    ofs << setw(40) << "Compressed trajectory file:" << setw(20) << xtcfile << endl;
 
     const string ndxfile = pt.get<std::string>("ndxfile","index.ndx");
     cout << "ndxfile = " << ndxfile << endl;
+    ofs << setw(40) << "Index file:" << setw(20) << ndxfile << endl;
 
     const int rand_n = strtol(pt.get<std::string>("rand_n","1000").c_str(), &endptr, 10);
     if (*endptr != ' ' && *endptr != 0)
@@ -97,6 +118,16 @@ int main(int argc, char* argv[])
         return -1;
     }
     cout << "rand_n = " << rand_n << endl;
+    ofs << setw(40) << "Insertions of particle per frame:" << setw(20) << rand_n  << endl;
+
+    const int block_n = strtol(pt.get<std::string>("block_n","5").c_str(), &endptr, 10);
+    if (*endptr != ' ' && *endptr != 0)
+    {
+        cout << "ERROR: 'block_n' must be an integer." << endl;
+        return -1;
+    }
+    cout << "block_n = " << block_n << endl;
+    ofs << setw(40) << "Blocks used in uncertainty analysis:" << setw(20) << block_n << endl;
 
     const double rcut = strtod(pt.get<std::string>("rcut","1.0").c_str(), &endptr);
     if (*endptr != ' ' && *endptr != 0)
@@ -105,6 +136,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     cout << "rcut = " << rcut << endl;
+    ofs << setw(40) << "Cutoff distance (nm):" << setw(20) << rcut << endl;
     const double rcut2 = rcut*rcut;
 
     const double T = strtod(pt.get<std::string>("T","298.15").c_str(), &endptr);
@@ -114,6 +146,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     cout << "T = " << T << endl;
+    ofs << setw(40) << "System temperature (K):" << setw(20) << T << endl;
     const double beta = 1.0/(R * T); // kJ/mol
 
     const int atomtypes = strtol(pt.get<std::string>("atomtypes","1").c_str(), &endptr, 10);
@@ -137,6 +170,11 @@ int main(int argc, char* argv[])
     iss >> randtype_sigma;
     iss >> randtype_epsilon;
 
+    ofs << setw(40) << "Test particle sigma (nm):" << setw(20) << randtype_sigma << endl;
+    ofs << setw(40) << "Test particle epsilon (kJ/mol):" << setw(20) << randtype_epsilon << endl;
+    ofs << setw(40) << "Number of atom types:" << setw(20) << atomtypes << endl;
+    ofs << setw(20) << "INDEX NAME" << setw(20) << "SIGMA (nm)" << setw(20) << "EPSILON (kJ/mol)" << setw(20) << "C6" << setw(20) << "C12" << endl;
+
     for (int i = 0; i < atomtypes; i++)
     {
         string atomtype_str = pt.get<std::string>("atomtype"+to_string(i+1));
@@ -151,6 +189,7 @@ int main(int argc, char* argv[])
         double sig = 0.5 * (randtype_sigma + atomtype_sigma);
         double c6 = 4.0 * eps * pow(sig, 6);
         double c12 = 4.0 * eps * pow(sig, 12);
+        ofs << setw(20) << atomtype_name << setw(20) << atomtype_sigma << setw(20) << atomtype_epsilon << setw(20) << c6 << setw(20) << c12 << endl;
         Atomtype at_tmp(atomtype_name, c6, c12);
         at.at(i) = at_tmp;
     }
@@ -159,6 +198,8 @@ int main(int argc, char* argv[])
 
     Trajectory trj(xtcfile, ndxfile);
     const int frame_n = trj.GetNFrames();
+
+    ofs << setw(40) << "Number of frames used:" << setw(20) << frame_n << endl;
 
     random_device rd;
     mt19937 gen(rd());
@@ -265,6 +306,18 @@ int main(int argc, char* argv[])
     chem_pot = -log(V_exp_pe_avg/V_avg) / beta;
 
     cout << chem_pot << " +/- " << sqrt(chem_pot_var) << " kJ / mol" << endl;
+
+    end = chrono::system_clock::now(); 
+    chrono::duration<double> elapsed_seconds = end-start;
+    time_t end_time = chrono::system_clock::to_time_t(end);
+    ofs << setw(40) << "Finished computation at:" << setw(20) << ctime(&end_time);
+    ofs << setw(40) << "Run time:" << setw(18) << elapsed_seconds.count() << " s" << endl;
+    ofs << "-----------------------------------------------------------------------" << endl;
+    ofs << "FINAL RESULT" << endl;
+    ofs << "-----------------------------------------------------------------------" << endl;
+    ofs << chem_pot << " +/- " << sqrt(chem_pot_var) << " kJ / mol" << endl;
+    ofs.close();
+
 
     return 0;
 
