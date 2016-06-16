59d8bfc23f0fed16b7fdc8cef52a3059454c5438
James W. Barnett <jbarnet4@tulane.edu>
2016-06-16 15:25:19 -0500

    move initializations outside of loop

--- Atomtype.cpp
+++ Atomtype.cpp
@@ -25,35 +25,39 @@ Atomtype::Atomtype(Trajectory &trj, string name, double c6, double c12, double r
 // Get the total PE contribution for a test particle for all atom of this atom type
 double Atomtype::CalcPE(int frame_i, Trajectory &trj, coordinates &rand_xyz, cubicbox_m256 &box, double vol)
 {
-    double pe = 0.0;
-    coordinates8 rand_xyz8(rand_xyz);
-    float result[n-8] __attribute__((aligned (16)));;
+    coordinates8 rand_xyz8(rand_xyz), atom_xyz;
+    __m256 r2_8, mask, r6, ri6, pe_tmp;
+    float result[n] __attribute__((aligned (16)));
     int atom_i = 0;
     for (; atom_i < this->n-8; atom_i+=8)
     {
-        coordinates8 atom_xyz = trj.GetXYZ8(frame_i, this->name, atom_i);
-        __m256 r2_8 = distance2(atom_xyz, rand_xyz8, box);
-        __m256 mask = _mm256_cmp_ps(r2_8, rcut2_8, _CMP_LT_OS);
-        __m256 r6 = _mm256_and_ps(mask, _mm256_mul_ps(_mm256_mul_ps(r2_8, r2_8), r2_8));
-        __m256 ri6 = _mm256_and_ps(mask, _mm256_div_ps(one,r6));
-        __m256 pe_tmp = _mm256_and_ps(mask, _mm256_mul_ps(ri6, _mm256_sub_ps(_mm256_mul_ps(c12_8, ri6), c6_8)));
+        atom_xyz = trj.GetXYZ8(frame_i, this->name, atom_i);
+        r2_8 = distance2(atom_xyz, rand_xyz8, box);
+        mask = _mm256_cmp_ps(r2_8, rcut2_8, _CMP_LT_OS);
+        r6 = _mm256_and_ps(mask, _mm256_mul_ps(_mm256_mul_ps(r2_8, r2_8), r2_8));
+        ri6 = _mm256_and_ps(mask, _mm256_div_ps(one,r6));
+        pe_tmp = _mm256_and_ps(mask, _mm256_mul_ps(ri6, _mm256_sub_ps(_mm256_mul_ps(c12_8, ri6), c6_8)));
         _mm256_store_ps(result+atom_i, pe_tmp);
     }
-    for (int i = 0; i < this->n-8; i++)
-    {
-        pe += result[i];
-    }
-    for (; atom_i < this->n; ++atom_i)
+    for (; atom_i < this->n; atom_i++)
     {
         coordinates atom_xyz = trj.GetXYZ(frame_i, this->name, atom_i);
         double r2 = distance2(atom_xyz, rand_xyz, cubicbox(box));
         if (r2 < this->rcut2)
         {
             double ri6 = 1.0/(pow(r2,3));
-            pe += ri6*(this->c12*ri6 - this->c6);
+            result[atom_i] = ri6*(this->c12*ri6 - this->c6);
+        }
+        else
+        {
+            result[atom_i] = 0.0;
         }
     }
-
+    double pe = 0.0;
+    for (int i = 0; i < this->n; i++)
+    {
+        pe += result[i];
+    }
 
     pe += this->n/vol * this->tail_factor;;
 
