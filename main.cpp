bf38a2569b0cf59eac77e9aa29f129b0fec74ca6
James W. Barnett <jbarnet4@tulane.edu>
2016-06-19 18:45:59 -0500

    Revert "rm some intermediate vars"
    This reverts commit f6802e995107bd6947e98be4acc0ec776e7e03c2.

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ class Atomtype {
             /* BEGIN SIMD SECTION */
 
             coordinates8 rand_xyz8(rand_xyz), atom_xyz;
-            __m256 r2_8, mask, ri6;
+            __m256 r2_8, mask, r6, ri6, pe_tmp;
             __m256 pe_sum = _mm256_setzero_ps();
             float result[n] __attribute__((aligned (16)));
 
@@ -81,8 +81,10 @@ class Atomtype {
                 atom_xyz = trj.GetXYZ8(frame_i, this->name, atom_i);
                 r2_8 = distance2(atom_xyz, rand_xyz8, box);
                 mask = _mm256_cmp_ps(r2_8, rcut2_8, _CMP_LT_OS);
-                ri6 = _mm256_and_ps(mask, _mm256_rcp_ps(_mm256_mul_ps(_mm256_mul_ps(r2_8, r2_8), r2_8)));
-                pe_sum = _mm256_and_ps(mask, _mm256_add_ps(_mm256_mul_ps(ri6, _mm256_sub_ps(_mm256_mul_ps(c12_8, ri6), c6_8)), pe_sum));
+                r6 = _mm256_and_ps(mask, _mm256_mul_ps(_mm256_mul_ps(r2_8, r2_8), r2_8));
+                ri6 = _mm256_and_ps(mask, _mm256_rcp_ps(r6));
+                pe_tmp = _mm256_and_ps(mask, _mm256_mul_ps(ri6, _mm256_sub_ps(_mm256_mul_ps(c12_8, ri6), c6_8)));
+                pe_sum = _mm256_add_ps(pe_tmp, pe_sum);
             }
             _mm256_store_ps(result, pe_sum);
             for (int i = 0; i < 8; i++)
